0f4652f4069c7eb955350655bf1a102191db1d23
VegadHarsh <harshvegad911@gmail.com>
2024-09-23 14:09:29 -0800

    Done with loader

--- JS/index.js
+++ JS/index.js
@@ -19,3 +19,41 @@ const hamburger = document.querySelector(".fa-bars");
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+document.addEventListener("click", (event) => {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnHamburger) {
+        menu.classList.remove("active");
+    }
+});
+
+
+// loding time
+document.addEventListener('DOMContentLoaded', function () {
+    // Show the loader on initial page load
+    document.getElementById('loader').style.display = 'flex';
+
+    // Hide the loader after 2 seconds
+    setTimeout(() => {
+        document.getElementById('loader').style.display = 'none';
+    }, 2000);
+
+    document.querySelectorAll('.nav-links a').forEach(link => {
+        link.addEventListener('click', function (event) {
+            const href = this.getAttribute('href');
+
+            
+            if (href === './Files/About.html' || href === '../Files/contact.html') {
+                event.preventDefault();
+
+                document.getElementById('loader').style.display = 'flex';
+
+                setTimeout(() => {
+                    window.location.href = href;
+                }, 1000);
+            }
+        });
+    });
+});
